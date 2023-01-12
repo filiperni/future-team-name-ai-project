dadf4704751eeabfac16c852fe6b8ed38592ff08
limmesi <85181244+limmesi@users.noreply.github.com>
2023-01-12 16:46:45 +0100

    Update najlblizsze_przystanki.py

--- najlblizsze_przystanki.py
+++ najlblizsze_przystanki.py
@@ -1,7 +1,8 @@
-import json
+from przystanek import Przystanek
 import matplotlib.pyplot as plt
 import numpy as np
-from przystanek import Przystanek
+import requests
+import json
 
 
 def plot(A, B, list1, list2=None):
@@ -20,8 +21,18 @@ def plot(A, B, list1, list2=None):
 
 
 def najb_przystanki(start_x, start_y, dest_x, dest_y):
-    lista_przystankow_json = json.load(open('lista_przystankow.json', encoding="utf-8"))
-    lista_przystankow = [Przystanek(stop["stopId"], stop["stopLon"], stop["stopLat"]) for stop in lista_przystankow_json["2023-01-12"]["stops"]]
+    url_lista_przystankow = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json"
+    try:
+        lista_przystankow_json = requests.get(url_lista_przystankow)
+    except:
+        print("Could not download data")
+
+    with open('data/lista_przystankow.json', 'w') as out_file:
+        json.dump(lista_przystankow_json.json(), out_file)
+
+    lista_przystankow_dict = json.load(open('data/lista_przystankow.json', encoding="utf-8"))
+    lista_przystankow = [Przystanek(stop["stopId"], stop["stopLon"], stop["stopLat"]) for stop in
+                         lista_przystankow_dict["2023-01-12"]["stops"]]
 
     start = Przystanek(00, start_x, start_y)
     dest = Przystanek(99, dest_x, dest_y)
@@ -33,8 +44,10 @@ def najb_przystanki(start_x, start_y, dest_x, dest_y):
     sorted_start = sorted(lista_przystankow, key=lambda x: x.dist_to_start)
     sorted_dest = sorted(lista_przystankow, key=lambda x: x.dist_to_dest)
 
-    # sorted_start_short = sorted_start[0:5]
-    # sorted_dest_short = sorted_dest[0:5]
-    # plot(start, dest, lista_przystankow, sorted_start_short+sorted_dest_short)
+    sorted_start_short = sorted_start[0:5]
+    sorted_dest_short = sorted_dest[0:5]
+    plot(start, dest, lista_przystankow, sorted_start_short + sorted_dest_short)
     return sorted_start, sorted_dest
 
+
+najb_przystanki(1, 1, 1, 1)
