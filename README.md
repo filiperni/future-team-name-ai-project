9069312ba76ebdbec841f35f93da045ee7d0ad4e
Filip Erni <64197585+filiperni@users.noreply.github.com>
2023-01-13 07:58:37 +0100

    tried adding blue walking paths

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 
 Hackathon ai project
 
+najlepiej otworzyć na macOS'sie
+
 Wazne!!
-wyłączyć cors w przeglądarce
+Wyłączyć Cors w przeglądarce żeby appka działała
+
+otworzyć folder z projektem w terminalu
+
+python3 venv .venv
+
+source .venv/bin/activate
+
+Pip install –upgrade pip
+
+Pip install -r requirements.txt
+
+uvicorn main:app --reload --timeout-keep-alive 120
+
+otworzyć plik stronka.html w przeglądarce
--- main.py
+++ main.py
@@ -16,9 +16,9 @@ async def root(posAx:float=0,posAy:float=0,posBx:float=0,posBy:float=0):
     
     
     
-    wynik = znajdz_linie((posAx,posAy),(posBx,posBy))
+    wspolzedne,odleglosci = znajdz_linie((posAx,posAy),(posBx,posBy))
     # wynik = znajdz_linie((18.64936,54.38486),(18.60782,54.40465))
-    print(wynik)
+    print(wspolzedne)
 
     #najblizsze przystanki
     #najblizsze_przystanki= get_najb_przystanki(posAx,posAy,posBx,posBy)
@@ -27,4 +27,4 @@ async def root(posAx:float=0,posAy:float=0,posBx:float=0,posBy:float=0):
 
 
 
-    return {"message": "Hello World","najblizsze_przystanki":wynik.wsp}
+    return {"message": "Hello World","najblizsze_przystanki":wspolzedne,"odleglosci_do_przyst_a_b":odleglosci}
