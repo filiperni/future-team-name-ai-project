9339cecf47e3434d01ea452e21e522d854f720c3
Filip Erni <64197585+filiperni@users.noreply.github.com>
2023-01-13 02:09:19 +0100

    Dragging points functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from fastapi import FastAPI
-from funkcje import najb_przystanki as get_najb_przystanki
+from funkcje import znajdz_linie
 
 
 app = FastAPI()
@@ -16,13 +16,14 @@ async def root(posAx:float=0,posAy:float=0,posBx:float=0,posBy:float=0):
     
     
     
-    print(posAx,posAy,posBx,posBy)
+    wynik = znajdz_linie()
+    print(wynik)
 
     #najblizsze przystanki
-    najblizsze_przystanki= get_najb_przystanki(posAx,posAy,posBx,posBy)
+    #najblizsze_przystanki= get_najb_przystanki(posAx,posAy,posBx,posBy)
 
     #dok
 
 
 
-    return {"message": "Hello World","najblizsze_przystanki":najblizsze_przystanki}
+    return {"message": "Hello World","najblizsze_przystanki":wynik}
